b8deccf958f985741ef8fb4bc8202bb311768794
MohanPrasathSece <mohanprasath.s2023ece@sece.ac.in>
2026-02-05 17:05:01 +0530

    Add Vercel Web Analytics

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,10 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { loadRazorpayCheckout } from "./lib/razorpay";
+import { Analytics } from "@vercel/analytics/react";
 
 // Preload Razorpay SDK to eliminate delay during checkout
 loadRazorpayCheckout().catch(() => {
   // Silently fail - will be retried when needed
 });
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
